151367db5720772464e25993398f0e028940751d
Jessica Nepomuceno <nepojessica_simoes@hotmail.com>
2024-03-14 18:27:01 -0300

    modificações-2 do projeto

--- src/scripts/main.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const movieItems = document.querySelectorAll('.movies__list__item');
-
-    movieItems.forEach(function (item) {
-        item.addEventListener('click', function () {
-            item.classList.toggle('clicked');
-        });
-    });
-});
