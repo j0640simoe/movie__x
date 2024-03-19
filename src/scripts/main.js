8139a94ebc565fcb9d2c8b6024e235ddbd25f935
Jessica Nepomuceno <nepojessica_simoes@hotmail.com>
2024-03-18 22:26:40 -0300

    mudanças no projeto

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,14 +1,28 @@
 const moviesList = document.querySelector('.movies__list');
-const moviesListItems = document.querySelectorAll('.movies__list__item');
-const totalMovies = moviesListItems.length;
-const carouselSpeed = 3000; 
 
-function nextSlide() {
-    currentIndex = (currentIndex + 1) % totalMovies;
-    updateCarousel();
-}
-function startCarousel() {
-    setInterval(nextSlide, carouselSpeed); 
+// Function para fazer a rolagem infinita
+function scrollMovies() {
+    const firstItem = moviesList.firstElementChild;
+    moviesList.appendChild(firstItem.cloneNode(true));
+    moviesList.removeChild(firstItem);
 }
 
-window.addEventListener('load', startCarousel);
+// Aqui você define o tempo em q vai ficar passando a rolagem. Coloquei 1 segundo por hora pra você ter uma visão melhor
+let autoScrollInterval = setInterval(scrollMovies, 1000);
+
+
+// Aqui ja vamos trabalhar com todas as sections series
+const seriesSections = document.querySelectorAll('.series');
+
+
+function scrollSeries() {
+    seriesSections.forEach(section => {
+        const seriesList = section.querySelector('.series__list');
+        const firstItem = seriesList.firstElementChild;
+        seriesList.appendChild(firstItem.cloneNode(true));
+        seriesList.removeChild(firstItem);
+    });
+}
+
+let autoScrollIntervalSeries = setInterval(scrollSeries, 1000);
+
